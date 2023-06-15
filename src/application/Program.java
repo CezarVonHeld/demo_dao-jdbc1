06e874de94561eaab6af9cf6577bf874bc4fdfe5
Cezar Von Held <cezarsa9@gmail.com>
2023-06-15 10:58:48 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21,"bob","bob@gmail.com", new Date(), 3000.0,obj);
 		
-		System.out.print(obj);
+		System.out.print(seller);
 
 	}
 
