48d16e55af11dccda1db054e62dd673644e5d12e
Cezar Von Held <cezarsa9@gmail.com>
2023-06-15 14:15:50 -0300

    update implement

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -93,8 +93,23 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			
+			if(rows==0) {
+				throw new DbException("Id not found");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException("Error" + e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}	
 	}
 
 	@Override
